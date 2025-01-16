4f224ea09e426d0ce600df5bc1ff863b51b179af
Blackman <DBlackman@int.co.chenango.ny.us>
2025-01-16 14:37:01 -0500

    modify project structure

--- public/information/projects.tsx
+++ public/information/projects.tsx
@@ -2,6 +2,7 @@ export const projects = [
     {
         id: 1,
         name: "Initiative Tracker - V1",
+        type: "Personal",
         shortDesc: "My first solo project - An Initiative Tracker for TTRPGs",
         longDesc: "This was the first project I ever built on my own. It takes an input, saves it to local strorage and generates information cards based on that information. This is a project that I am actively changing. Version 2 will make use of some different technologies that I didn't know how to use back then.",
         tech: ["HTML", "CSS", "Vanilla Javascript", "Javascript Modules"],
