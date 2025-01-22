fae9730d291de01dc85bfb9a2aa341f0e8c1e34f
Blackman <DBlackman@int.co.chenango.ny.us>
2025-01-22 13:10:37 -0500

    add elements to homepage

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header>
-            <h1>David Blackmans Portfolio</h1>
+            <h1>David Blackmans Portfolio - work in process</h1>
         </header>
     )
 }
